74b2b7bdb82a4db4c386e1666db06477e250e19b
chiragkr04 <chiragkumar6921@gmail.com>
2023-01-24 18:12:23 +0530

    added login api + result model

--- backend/src/main/java/com/library/demo/DemoApplication.java
+++ backend/src/main/java/com/library/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.library.library_prg;
+package com.library.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LibraryPrgApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryPrgApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
---
+++ backend/src/main/java/com/library/demo/service/EmailService.java
@@ -0,0 +1,26 @@
+package com.library.demo.service;
+
+import com.library.demo.model.UserCredential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Autowired
+    private PasswordGenerator passwordGenerator;
+    public void sendEmail(UserCredential user){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("applicationtest0001@gmail.com");
+        simpleMailMessage.setTo(user.getEmailId());
+        simpleMailMessage.setSubject("Credential");
+        simpleMailMessage.setText("Hi " +user.getFirstname()
+                + "\nThese are your credentials.\n"
+                + "\nUser Id: " + user.getUserId()
+                + "\nPassword: " + user.getPassword());
+        javaMailSender.send(simpleMailMessage);
+    }
+}
