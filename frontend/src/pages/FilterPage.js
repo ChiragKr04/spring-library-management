4bb4cc91f474d7dc084a3d794ae709a1c0c8f615
chiragkr04 <chiragkumar6921@gmail.com>
2023-02-20 12:06:46 +0530

    added multi genre filter

--- frontend/src/pages/FilterPage.js
+++ frontend/src/pages/FilterPage.js
@@ -1,5 +1,5 @@
-import { Close, Filter1, FilterAlt, Search } from '@mui/icons-material';
-import { Button, Card, Fab, Grid, IconButton, MenuItem, TextField } from '@mui/material'
+import { Cancel, Close, CloseOutlined, Delete, Filter1, FilterAlt, Search } from '@mui/icons-material';
+import { Button, Card, Chip, Fab, Grid, IconButton, MenuItem, Stack, TextField } from '@mui/material'
 import { Box } from '@mui/system';
 import React from 'react'
 
@@ -9,9 +9,10 @@ export default function FilterPage({
   changeBookDataOnFilter,
   setDataOnClearFilter,
 }) {
-  const [categoryText, setCategoryText] = React.useState("all");
+  const [categoryText, setCategoryText] = React.useState("");
   const [ratingText, setRatingText] = React.useState("none");
   const [isFilterEnable, setFilterEnable] = React.useState(false);
+  const [allGenre, setAllGenre] = React.useState([]);
 
 
   const ratingValue = [
@@ -37,21 +38,43 @@ export default function FilterPage({
     },
   ];
 
+  const divMidItemStyle = {
+    width: "100%",
+    margin: "4px",
+    flex: 2
+  };
+
+  const divFLItemStyle = {
+    width: "100%",
+    margin: "4px",
+    flex: 1
+  };
+
   const doFiltering = async () => {
     console.log(`category text you typed ${categoryText}`);
     console.log(`rating you selected ${ratingText}`);
 
+    const isInAllGenre = (currGenre) => {
+      if (allGenre.length == 0) {
+        return true;
+      }
+      const lowerCaseList = allGenre.map((item) => item.toLowerCase());
+      return lowerCaseList.includes(currGenre.toLowerCase());
+    };
+
     bookDataCopy = mainBookData.filter((item) => {
       const selectedGenre =
-        categoryText.toLowerCase === "all" || categoryText === ""
+        categoryText.toLowerCase() === "all" || categoryText === ""
           ? "none"
           : categoryText;
       const selectedRating =
         ratingText === "none" || ratingText === ""
           ? "none"
-          : parseInt(ratingText)
-      if ((selectedGenre.toLowerCase() === "none" || item.genre.toLowerCase() === selectedGenre.toLowerCase())
-        && (selectedRating === "none" || item.rating >= selectedRating)) {
+          : parseInt(ratingText);
+      if (
+        (isInAllGenre(item.genre)) &&
+        (selectedRating === "none" || item.rating >= selectedRating)
+      ) {
         return true;
       }
       return false;
@@ -65,6 +88,37 @@ export default function FilterPage({
     setFilterEnable(false);
     setDataOnClearFilter();
   }
+
+  const removeGenre = (index) => {
+    var newGenres = [...allGenre];
+    newGenres.splice(index, 1);
+    setAllGenre(newGenres);
+    // doFiltering();
+  };
+
+  const doOnEnter = (event) => {
+    if (event.key === "Enter") {
+      if (categoryText != "") {
+        let newGenres = [...allGenre];
+        const lowerCaseList = newGenres.map((item) => item.toLowerCase());
+        if (!lowerCaseList.includes(categoryText.toLowerCase())) {
+          newGenres.push(categoryText);
+          setAllGenre(newGenres);
+          // doFiltering();
+        }
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    doFiltering();
+  }, [ratingText]);
+
+  React.useEffect(() => {
+    console.log(`selectedGenre ${categoryText}`);
+    doFiltering();
+    setCategoryText("");
+  }, [allGenre]);
 
   return !isFilterEnable ? (
     <div style={{
@@ -93,11 +147,16 @@ export default function FilterPage({
           padding: "12px"
         }}
       >
-        <Grid
-          container
-          justifyContent={"space-evenly"}
-        >
-          <Grid item >
+        <Box style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          width: '100%',
+        }}>
+          {/* Item 1 */}
+          <div style={{
+            margin: "4px",
+            flex: 1
+          }}>
             <Button
               pr={2}
               variant='contained'
@@ -105,64 +164,99 @@ export default function FilterPage({
               disabled
               style={{
                 backgroundColor: "rgb(25,118,210)",
+                width: "100%",
                 color: "white"
               }}
             >
               Filter
             </Button>
-          </Grid>
-          <Grid item >
-            <TextField
-              variant="outlined"
-              size='small'
-              label="Genre"
-              onChange={(event) => {
-                setCategoryText(event.target.value);
-              }}
-              style={{
-                paddingLeft: "5px",
-                paddingRight: "5px"
-              }}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              select
-              label="Rating"
-              size='small'
-              defaultValue="none"
-              onChange={(event) => {
-                setRatingText(event.target.value);
-              }}
-            >
-              {ratingValue.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-          <Grid item>
-            <Button
-              variant='contained'
-              onClick={() => {
-                doFiltering();
-              }}
-              startIcon={<Search />}
-            >
-              Search
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button
-              variant='outlined'
-              startIcon={<Close />}
-              onClick={clearFilters}
-            >
-              Clear Filter
-            </Button>
-          </Grid>
-        </Grid>
+          </div>
+          {/* Item 2 */}
+          <div style={{
+            width: "100%",
+            margin: "4px",
+            flex: 2
+          }}>
+            <Grid item>
+              <Stack>
+                <TextField
+                  variant="outlined"
+                  size='small'
+                  value={categoryText}
+                  label="Genre"
+                  onChange={(event) => {
+                    setCategoryText(event.target.value);
+                  }}
+                  onKeyDown={doOnEnter}
+                  style={{
+                    paddingLeft: "5px",
+                    paddingRight: "5px"
+                  }}
+                />
+                <div style={{ paddingTop: "5px" }}></div>
+                <Grid container spacing={1}>
+                  {allGenre.map((genre, index) => (
+                    <Grid item xs={4} key={index}>
+                      <Chip
+                        label={genre}
+                        onDelete={() => { removeGenre(index) }}
+                        deleteIcon={<Cancel style={{ color: "white" }} />}
+                        style={{
+                          backgroundColor: "#D61355",
+                          color: "white",
+                        }}
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Stack>
+            </Grid>
+          </div>
+          {/* Item 3 */}
+          <div style={{
+            margin: "4px",
+            flex: 2
+          }}>
+            <Grid item>
+              <TextField
+                select
+                label="Rating"
+                size='small'
+                defaultValue="none"
+                style={{
+                  width: "100%"
+                }}
+                onChange={(event) => {
+                  setRatingText(event.target.value);
+                }}
+              >
+                {ratingValue.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </div>
+          {/* Item 4 */}
+          <div style={{
+            margin: "4px",
+            flex: 1
+          }}>
+            <Grid item>
+              <Button
+                variant='outlined'
+                style={{
+                  width: "100%",
+                }}
+                startIcon={<Close />}
+                onClick={clearFilters}
+              >
+                Clear Filter
+              </Button>
+            </Grid>
+          </div>
+        </Box>
       </Card>
     </div>
   )
